f0c2f3ddea7e0a730a531ad602e4e51b9d4d9e0a
Jonathan Jefferies <jonathan.jefferies@supergroup.co.uk>
2013-06-06 13:55:23 +0100

    Split out some tests.

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -8,12 +8,28 @@ class MockConfig extends \jjok\Config\Config {}
 class ConfigTest extends PHPUnit_Framework_TestCase {
 
 	/**
+	 * @covers \jjok\Config\Config::set
+	 */
+	public function testSetReturnsSelf() {
+		$config = new \jjok\Config\Config();
+		
+		$this->assertInstanceOf('jjok\Config\Config',
+			$config->set('test1', 'test1')
+		);
+		$this->assertInstanceOf('jjok\Config\Config',
+			$config->set('test2', 'test2')
+				   ->set('test3', 'test3')
+		);
+	}
+	
+	/**
 	 * @covers \jjok\Config\Config::get
 	 * @covers \jjok\Config\Config::set
 	 */
-	public function testConfigValuesCanBeSetAndGot() {
+	public function testSetValuesCanBeGot() {
 		$config = new \jjok\Config\Config();
 
+		$test_string = 'Some string value';
 		$test_integer = 1234567890;
 		$test_float = 12345.67890;
 		$test_object = new \jjok\Config\Config();
@@ -22,17 +38,14 @@ class ConfigTest extends PHPUnit_Framework_TestCase {
 			'blah'
 		);
 
-		$this->assertInstanceOf('jjok\Config\Config',
-			$config->set('a string', 'Some string value')
-		);
-		$this->assertInstanceOf('jjok\Config\Config',
-			$config->set('an integer', $test_integer)
-				   ->set('a float', $test_float)
-				   ->set('an array', $test_array)
-			       ->set('an object', $test_object)
-		);
+		$config->set('a string', $test_string);
+		
+		$config->set('an integer', $test_integer)
+		       ->set('a float', $test_float)
+		       ->set('an array', $test_array)
+		       ->set('an object', $test_object);
 
-		$this->assertSame('Some string value', $config->get('a string'));
+		$this->assertSame($test_string, $config->get('a string'));
 		$this->assertSame($test_integer, $config->get('an integer'));
 		$this->assertSame($test_float, $config->get('a float'));
 		$this->assertSame($test_array, $config->get('an array'));
